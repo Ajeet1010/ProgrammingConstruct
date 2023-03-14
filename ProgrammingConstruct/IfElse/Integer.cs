a37aaf6e38af832217d2cb8462d5c741ae638f0f
Ajeet <ajeetpal101999@gmail.com>
2023-03-15 00:55:22 +0530

    [Ajeet] If else statemnt

---
+++ ProgrammingConstruct/IfElse/Integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct.IfElse
+{
+    // UC1-  Program to accept two integers and check whether they are equal or not.
+    internal class Integer
+    {
+        public void Equals()
+        {
+            Console.Write("Enter first integer:  ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second integer:  ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            // checking input integers are equals or not.
+            if (a == b)
+                Console.WriteLine("Integers are Equals");
+            else
+                Console.WriteLine("Integers are not Equals");
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ProgrammingConstruct
+﻿using ProgrammingConstruct.IfElse;
+
+namespace ProgrammingConstruct
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("*****  Programming Construct Problems  *****");
+            Integer integer = new Integer();
+            integer.Equals();
         }
     }
 }
