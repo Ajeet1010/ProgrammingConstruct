0894b7bf778f72d74570411abbd4f8f7e8905caa
Ajeet <ajeetpal101999@gmail.com>
2023-03-15 01:23:49 +0530

    [Ajeet] Find largest no among three no's

---
+++ ProgrammingConstruct/Largest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    // UC4 - Program to find the largest of three numbers..
+    internal class Largest
+    {
+        public void No()
+        {
+            Console.Write("Enter first no:  ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second no:  ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third no:  ");
+            int third = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entered three no are: {0}, {1}, {2}",first, second, third);
+
+            // checking the largest no.
+            if (first > second && first > third)
+                Console.WriteLine("\nFirst no is largest among three: " + first);
+            else if (second > first && second > third)
+                Console.WriteLine("\nSecond no is largest among three: " + second);
+            else
+                Console.WriteLine("\nThird no is largest among three: " + third);
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -6,9 +6,9 @@ namespace ProgrammingConstruct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*****  Programming Construct Problems  *****");
-            Eligibility eligibility = new Eligibility();
-            eligibility.CastVote();
+            Console.WriteLine("*****  Programming Construct Problem  *****");
+            Largest largest = new Largest();
+            largest.No();
         }
     }
 }
