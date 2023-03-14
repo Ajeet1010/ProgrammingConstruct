ee2153a6f09da9f8aaffd40b769476d763b8c754
Ajeet <ajeetpal101999@gmail.com>
2023-03-15 00:42:10 +0530

    [Ajeet] If & Else Problem statement

---
+++ ProgrammingConstruct/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingConstruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  Programming Construct Problems  *****");
+        }
+    }
+}
