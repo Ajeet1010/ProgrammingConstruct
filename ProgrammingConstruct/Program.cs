82ad8a953f97068a57e16cff029c2cdd48e95ed9
Ajeet <ajeetpal101999@gmail.com>
2023-03-27 16:22:02 +0530

    [Ajeet] Using while loop Toss a coin.

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -7,8 +7,8 @@ namespace ProgrammingConstruct
         static void Main(string[] args)
         {
             Console.WriteLine("*****  Programming Construct Problem  *****");
-            ForLoop forLoop = new ForLoop();
-            forLoop.SumOfSquare();
+            WhileLoop whileLoop = new WhileLoop();
+            whileLoop.FlipCoin();
         }
     }
 }
---
+++ ProgrammingConstruct/WhileLoop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    // UC11: Program to Flip Coin problem till either Heads or Tails wins 20 times.
+    internal class WhileLoop
+    {
+        public void FlipCoin()
+        {
+            Random random = new Random();
+            int heads = 0;
+            int tails = 0;
+
+            while (heads < 20 && tails < 20)
+            {
+                int toss = random.Next(2);
+                if (toss == 0)
+                {
+                    heads++;
+                    Console.WriteLine("Heads");
+                }
+                else
+                {
+                    tails++;
+                    Console.WriteLine("Tails");
+                }
+                
+            }
+
+            if (heads == 20)
+            {
+                Console.WriteLine("Head comes 20 times\nHeads wins!");
+            }
+            else
+            {
+                Console.WriteLine("Tail comes 20 times\nTails wins!");
+            }
+        }
+
+    }
+}
